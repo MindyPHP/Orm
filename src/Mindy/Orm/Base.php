f2f5d3d8fe1c44c632b20eeac0627a396fe4771d
Anton Okulov <qant@studio107.ru>
2014-02-26 17:44:04 +0400

    Base

---
+++ src/Mindy/Orm/Base.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * All rights reserved.
+ *
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 03/01/14.01.2014 22:10
+ */
+
+namespace Mindy\Orm;
+
+
+use ArrayAccess;
+use Exception;
+
+use Mindy\Query\Connection;
+
+use Mindy\Orm\Traits\Fields;
+use Mindy\Query\Query;
+
+class Base implements ArrayAccess
+{
+    /**
+     * @return string the fully qualified name of this class.
+     */
+    public static function className()
+    {
+        return get_called_class();
+    }
+
+    /**
+     * Return table name based on this class name.
+     * Override this method for custom table name.
+     * @return string
+     */
+    public static function tableName()
+    {
+        $className = get_called_class();
+        $normalizeClass = rtrim(str_replace('\\', '/', $className), '/\\');
+        if (($pos = mb_strrpos($normalizeClass, '/')) !== false) {
+            $class = mb_substr($normalizeClass, $pos + 1);
+        } else {
+            $class = $normalizeClass;
+        }
+        return trim(strtolower(preg_replace('/(?<![A-Z])[A-Z]/', '_\0', $class)), '_');
+    }
+
+    /**
+     * Returns whether there is an element at the specified offset.
+     * This method is required by the SPL interface `ArrayAccess`.
+     * It is implicitly called when you use something like `isset($model[$offset])`.
+     * @param mixed $offset the offset to check on
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return $this->$offset !== null;
+    }
+
+    /**
+     * Returns the element at the specified offset.
+     * This method is required by the SPL interface `ArrayAccess`.
+     * It is implicitly called when you use something like `$value = $model[$offset];`.
+     * @param mixed $offset the offset to retrieve element.
+     * @return mixed the element at the offset, null if no element is found at the offset
+     */
+    public function offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+
+    /**
+     * Sets the element at the specified offset.
+     * This method is required by the SPL interface `ArrayAccess`.
+     * It is implicitly called when you use something like `$model[$offset] = $item;`.
+     * @param integer $offset the offset to set element
+     * @param mixed $item the element value
+     */
+    public function offsetSet($offset, $item)
+    {
+        $this->$offset = $item;
+    }
+
+    /**
+     * Sets the element value at the specified offset to null.
+     * This method is required by the SPL interface `ArrayAccess`.
+     * It is implicitly called when you use something like `unset($model[$offset])`.
+     * @param mixed $offset the offset to unset element
+     */
+    public function offsetUnset($offset)
+    {
+        $this->$offset = null;
+    }
+}
