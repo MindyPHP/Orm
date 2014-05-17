ca7994c216d93f45a577030a88dd8536f715a34c
Falaleev Maxim <max@studio107.ru>
2014-05-17 16:42:47 +0400

    BlobField todo

--- src/Mindy/Orm/Fields/BlobField.php
+++ src/Mindy/Orm/Fields/BlobField.php
@@ -14,12 +14,19 @@
 
 namespace Mindy\Orm\Fields;
 
-
 class BlobField extends Field
 {
     public function sqlType()
     {
         return 'longblob';
+    }
+
+    public function getDbPrepValue()
+    {
+        // TODO добавить в BlobField
+        // if ($db->getDriverName() == 'sqlsrv' || $db->getDriverName() == 'mssql' || $db->getDriverName() == 'dblib')
+        //     $select = 'CONVERT(VARCHAR(MAX), data)';
+        return parent::getDbPrepValue();
     }
 }
 
