c341965a79c909230cac3c68c8ee2a4e83300023
Falaleev Maxim <max@studio107.ru>
2014-05-19 15:11:27 +0400

    Move to other project

---
+++ src/Mindy/Orm/Fields/FloatField.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 
+ *
+ * All rights reserved.
+ * 
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 19/05/14.05.2014 13:54
+ */
+
+namespace Mindy\Orm\Fields;
+
+
+class FloatField 
+{
+
+}
+
