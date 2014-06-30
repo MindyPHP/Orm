5b2aef0c969d221112fef1b5f63da87b70d7b5aa
Falaleev Maxim <max@studio107.ru>
2014-06-30 21:14:32 +0400

    Add example config file

---
+++ Tests/config_local.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'mysql' => [
+        'dsn' => 'mysql:host=localhost;dbname=tmp',
+        'username' => 'root',
+        'password' => '123456',
+        'charset' => 'utf8',
+    ],
+    'sqlite' => [
+        'dsn' => 'sqlite::memory:',
+    ]
+];
