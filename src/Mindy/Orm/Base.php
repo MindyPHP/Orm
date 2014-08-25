42fca0007348ac73c247d83de6e24cb90c54bfe8
Falaleev Maxim <max@studio107.ru>
2014-08-25 10:26:29 +0400

    Remove unused code

--- src/Mindy/Orm/Base.php
+++ src/Mindy/Orm/Base.php
@@ -1283,12 +1283,9 @@ abstract class Base implements ArrayAccess
         return static::getMeta()->getManyFields();
     }
 
-    public function delete($sql = false)
+    public function delete()
     {
         $this->onBeforeDeleteInternal();
-        if($sql) {
-            return $this->objects()->deleteSql(['pk' => $this->pk]);
-        }
         $result = $this->objects()->delete(['pk' => $this->pk]);
         if ($result >= 1) {
             $this->onAfterDeleteInternal();
