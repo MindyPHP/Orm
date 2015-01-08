b3de7357772fb5b30b8be4a0394e6eedc0469532
Falaleev Maxim <max@studio107.ru>
2015-01-08 15:53:29 +0300

    up

---
+++ Tests/TestMigrate.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * All rights reserved.
+ *
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 08/01/15 14:59
+ */
+
+namespace Tests;
+
+use Mindy\Query\Migration;
+
+class TestMigrate extends Migration
+{
+
+}
+ 
