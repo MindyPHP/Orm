0e2075c8488dcd52965103413efbb89b019a57b5
Falaleev Maxim <max@studio107.ru>
2015-02-05 14:19:42 +0300

    Travis =(

--- Tests/config_local.php
+++ Tests/config_local.php
@@ -5,6 +5,7 @@ return [
         'class' => '\Mindy\Query\Connection',
         'dsn' => 'mysql:host=localhost;dbname=tmp',
         'username' => 'root',
+        'password' => '123456',
         'charset' => 'utf8',
     ],
     'sqlite' => [
--- Tests/config_travis.php
+++ Tests/config_travis.php
@@ -5,6 +5,7 @@ return [
         'class' => '\Mindy\Query\Connection',
         'dsn' => 'mysql:host=127.0.0.1;dbname=test',
         'username' => 'root',
+        'password' => '123456',
         'charset' => 'utf8',
     ],
     'sqlite' => [
