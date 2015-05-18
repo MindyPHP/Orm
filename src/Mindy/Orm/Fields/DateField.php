02764268cdf79c1c9bd4a4bbeb96c6ae11d83dac
Falaleev Maxim <max@studio107.ru>
2015-05-18 05:43:38 +0300

    Fix DateField. Added autoNow and autoNowAdd property.

--- src/Mindy/Orm/Fields/DateField.php
+++ src/Mindy/Orm/Fields/DateField.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Mindy\Orm\Fields;
+use Mindy\Query\ConnectionManager;
 
 /**
  * Class DateField
@@ -10,14 +11,47 @@ class DateField extends Field
 {
     public $autoFetch = true;
 
+    public $autoNowAdd = false;
+
+    public $autoNow = false;
+
     public function sqlType()
     {
         return 'date';
     }
 
+    public function onBeforeInsert()
+    {
+        if ($this->autoNowAdd) {
+            $this->getModel()->setAttribute($this->name, $this->getValue());
+        }
+    }
+
+    public function onBeforeUpdate()
+    {
+        if ($this->autoNow) {
+            $this->getModel()->setAttribute($this->name, $this->getValue());
+        }
+    }
+
+    public function getValue()
+    {
+        $db = ConnectionManager::getDb()->getQueryBuilder();
+        if (is_numeric($this->value) || $this->autoNowAdd && $this->getModel()->getIsNewRecord() || $this->autoNow) {
+            return $db->convertToDate($this->value);
+        } else {
+            return $this->value;
+        }
+    }
+
     public function sqlDefault()
     {
         return $this->default === null ? '' : "DEFAULT '{$this->default}'";
+    }
+
+    public function sqlNullable()
+    {
+        return $this->autoNow ? 'NULL' : parent::sqlNullable();
     }
 
     public function getFormField($form, $fieldClass = null, array $extra = [])
