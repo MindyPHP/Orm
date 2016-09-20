73c437dc015256c2df4afd2c362d780482fb8d07
Maksim Falaleev <max@studio107.ru>
2016-09-20 22:10:37 +0300

    Allow to load file from fs

---
+++ test/config_local.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'mysql' => [
+        'dbname' => 'test',
+        'user' => 'root',
+        'password' => '',
+        'host' => 'localhost',
+        'driver' => 'pdo_mysql',
+    ],
+    'sqlite' => [
+        'memory' => true,
+        'driver' => 'pdo_sqlite',
+        'driverClass' => 'Mindy\QueryBuilder\Driver\SqliteDriver',
+    ],
+    'pgsql' => [
+        'dbname' => 'test',
+        'user' => 'root',
+        'password' => '',
+        'host' => 'localhost',
+        'driver' => 'pdo_pgsql',
+    ]
+];
