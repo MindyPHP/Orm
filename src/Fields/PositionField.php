15c28bc9f967b75e7b71d90c6be228b2f428ef4e
Falaleev Maxim <max@studio107.ru>
2016-09-29 00:25:03 +0300

    Move and fix PositionField from Core module

---
+++ src/Fields/PositionField.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 28/09/16
+ * Time: 22:56
+ */
+
+namespace Mindy\Orm\Fields;
+
+use Mindy\Orm\ModelInterface;
+use Mindy\Orm\TreeModel;
+
+/**
+ * Class PositionField
+ * @package Modules\Core\Fields\Orm
+ */
+class PositionField extends IntField
+{
+    /**
+     * @param ModelInterface $model
+     * @param $value
+     */
+    public function beforeInsert(ModelInterface $model, $value)
+    {
+        if (is_null($value) || $value === '') {
+            $model->setAttribute($this->getName(), $this->getNextPosition($model));
+        }
+    }
+
+    /**
+     * @param ModelInterface $model
+     * @return int
+     */
+    public function getNextPosition(ModelInterface $model) : int
+    {
+        $qs = $model->objects();
+        if ($model instanceof TreeModel && !empty($model->parent_id)) {
+            $qs->filter(['parent_id' => $model->parent_id]);
+        }
+
+        $max = (int)$qs->max($this->getName());
+        return $max ? $max + 1 : 1;
+    }
+}
