07aeb0c3e187681d7b55ecfef135fb56168dcb03
Falaleev Maxim <max@studio107.ru>
2017-12-25 22:26:49 +0300

    up

--- AttributeCollection.php
+++ AttributeCollection.php
@@ -12,9 +12,9 @@ declare(strict_types=1);
 
 namespace Mindy\Orm;
 
-use Doctrine\Common\Collections\ArrayCollection;
+use ArrayAccess;
 
-class AttributeCollection
+class AttributeCollection implements ArrayAccess
 {
     /**
      * @var array
@@ -25,9 +25,170 @@ class AttributeCollection
      */
     protected $oldAttributes = [];
 
-    public function __construct(array $attributes = [])
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function __set($name, $value)
     {
-        $this->attributes = new ArrayCollection($attributes);
-        $this->oldAttributes = new ArrayCollection();
+        $this->setAttribute($name, $value);
+    }
+
+    /**
+     * @param $name
+     *
+     * @return mixed|null
+     */
+    public function __get($name)
+    {
+        return $this->getAttribute($name);
+    }
+
+    /**
+     * @param $name
+     *
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return $this->hasAttribute($name);
+    }
+
+    /**
+     * @param $name
+     *
+     * @return bool
+     */
+    public function hasAttribute($name)
+    {
+        return array_key_exists($name, $this->attributes);
+    }
+
+    /**
+     * @param $name
+     *
+     * @return string|int|null
+     */
+    public function getAttribute($name)
+    {
+        return isset($this->attributes[$name]) ? $this->attributes[$name] : null;
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function setAttribute($name, $value)
+    {
+        $this->oldAttributes[$name] = $this->getAttribute($name);
+        $this->attributes[$name] = $value;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * @param $name
+     *
+     * @return mixed|null
+     */
+    public function getOldAttribute($name)
+    {
+        return isset($this->oldAttributes[$name]) ? $this->oldAttributes[$name] : null;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOldAttributes()
+    {
+        return $this->oldAttributes;
+    }
+
+    /**
+     * Clear old attributes.
+     */
+    public function resetOldAttributes()
+    {
+        $this->oldAttributes = [];
+    }
+
+    /**
+     * @return array
+     */
+    public function getDirtyAttributes()
+    {
+        return array_keys($this->getOldAttributes());
+    }
+
+    /**
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->attributes);
+    }
+
+    /**
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        if (array_key_exists($offset, $this->attributes)) {
+            return $this->attributes[$offset];
+        }
+
+        return null;
+    }
+
+    /**
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->attributes[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->attributes[$offset]);
     }
 }
